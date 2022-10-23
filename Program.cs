212ad59ecdb665140a382bc5df48b3bc1be3c635
Khatanboyev Elbek <programmerelbek@gmail.com>
2022-10-23 03:11:01 -0700

    INFRA: Main features

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            
+            int keschaAge = 3;
+            Console.Write("Type your name: ");
+            string? firstName = Console.ReadLine();
+            Console.Write($"Hey {firstName}! Type your age: ");
+            int? age = Convert.ToInt32(Console.ReadLine());
+            int? ageDifference = age - keschaAge;
+            Console.WriteLine($"The difference between {firstName}'s and kescha's age is {ageDifference}");
         }
     }
 }
